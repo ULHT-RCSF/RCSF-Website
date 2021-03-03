9417fc2bebbe6eed0aed0961553591694e00a774
Ricardo Santos <a21904332@alunos.ulht.pt>
2021-03-03 11:02:33 +0000

    Primeiro Commit

---
+++ app/routes.py
@@ -0,0 +1,33 @@
+from app import app
+from flask import render_template, request
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/gsm')
+def gsm():
+    return render_template('gsm.html')
+
+
+@app.route('/gsm-r')
+def gsmr():
+    return render_template('gsm-r.html')
+
+
+@app.route('/umts')
+def umts():
+    return render_template('umts.html')
+
+
+@app.route('/lte')
+def lte():
+    return render_template('lte.html')
+
+
+@app.route("/sobre")
+def sobre():
+    return render_template('sobre.html')
